5055843f75a3f3427069be72a3b5c221d438d8c5
limu2030 <limu2030@gmail.com>
2022-04-25 07:26:18 +0300

    create user class

---
+++ User.py
@@ -0,0 +1,12 @@
+from argparse import _AppendConstAction
+
+
+class User:
+    user_menu=[]
+    def __init__(self, firstname, lastname, username, password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+
+     
