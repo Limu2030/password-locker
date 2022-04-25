1328df04055b217695b71e3c5d3f9d4a13b65048
limu2030 <limu2030@gmail.com>
2022-04-25 08:28:34 +0300

    function to save new app details

--- Credentials.py
+++ Credentials.py
@@ -1,2 +1,9 @@
 class Credentials: 
     app_details = []
+    def __init__(self, appname,appusername, apppassword):
+        self.appname = appname
+        self.appusername = appusername
+        self.apppassword = apppassword
+
+    def save_Credentials(self):
+        Credentials.app_details.append(self)    
